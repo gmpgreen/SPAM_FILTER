cfbada02be412f68cf28f0303b8423d54e959f6c
gmpgreen <gmpgreen16@gmail.com>
2019-07-19 19:50:12 -0700

    support for crossover, mutation, and inversion

--- ga.cpp
+++ ga.cpp
@@ -48,6 +48,7 @@ Param In: sms_data data, contains the two vectors of sms test data in lower case
 */
 GA::GA(sms_data data)
 {
+    /* Initialization of population */
     mData = data;
     for (int i = 0; i < N; i++)
     {
@@ -61,8 +62,140 @@ GA::GA(sms_data data)
         /* For each population member, eval fitness */
         mPopulation[i].fitness = Fitness_func(mPopulation[i], data);
 
+        std::cout << "inital generation" <<std::endl;
         std::cout << "fitness: " << mPopulation[i].fitness << std::endl;
     }
+    
+    
+    for (int k = 0; k < NUM_GENERATIONS; k++)
+    {
+        std::cout << "generation " << k+1 << ":" << std::endl;
+
+        /* Crossover */
+        for (int i = 0; i < N; i+=2)
+        {
+            Crossover(Selection(), Selection(), i, i + 1);
+        }
+        for (int i = 0; i < N; i++)
+        {
+            mPopulation[i] = mPopulationTemp[i];
+        }
+
+        Mutation();
+        Inversion();
+
+        fitness_func_wrapper();
+
+        for (int i = 0; i < N; i++)
+        {
+            std::cout << "fitness: " << mPopulation[i].fitness << std::endl;
+        }
+    }
+}
+
+void GA::Mutation()
+{
+    for (int i = 0; i < N; i++)
+    {
+        if (rand()%2) //50% of the time, flip a random bit
+        {
+            mPopulation[N].features[rand()%16].on_off = !(mPopulation[N].features[rand()%16].on_off);
+        }
+    }
+}
+
+void GA::Inversion()
+{
+    for (int i = 0; i < N; i++)
+    {
+        int inv_start = rand()%16;
+        int inv_end = rand()%16;
+
+        if (inv_start < inv_end)
+        {
+            population temp = mPopulation[i];
+            
+            for (int j = inv_start; j < inv_end; j++)
+            {
+                temp.features[j] = mPopulation[i].features[inv_start - j + inv_end];
+            }
+            mPopulation[i] = temp;
+        }
+    }
+}
+
+/* 
+ * Param In: index_p1 parent index 1
+ *           index_p2 parent index 2
+ *           index_c1 child index 1
+ *           index_c2 child index 2  
+*/
+void GA::Crossover(int index_p1, int index_p2, int index_c1, int index_c2)
+{
+    population pop1;
+    population pop2;
+
+    pop1.fitness = 0; //Init to zero
+    pop2.fitness = 0;
+
+    int half_chromosome  = W/2;
+    
+    for (int j = 0; j < W; j++)
+    {
+        if (j < half_chromosome)
+        {
+            //1st half of chromosome 1 stays in 1st half of new chromosome 1
+            pop1.features[j].order_id_index = mPopulation[index_p1].features[j].order_id_index;
+            pop1.features[j].on_off = mPopulation[index_p1].features[j].on_off;
+
+            //2nd half of chromosome 1 goes in 1st half of new chromosome 2
+            pop2.features[j].order_id_index = mPopulation[index_p1].features[j + half_chromosome].order_id_index;
+            pop2.features[j].on_off = mPopulation[index_p1].features[j + half_chromosome].on_off;
+        }
+        else
+        {
+            //1st half of chromosome 2 goes in 2nd half of new chromosome 1
+            pop1.features[j].order_id_index = mPopulation[index_p2].features[j - half_chromosome].order_id_index;
+            pop1.features[j].on_off = mPopulation[index_p2].features[j - half_chromosome].on_off;
+
+            //2nd half of chromosome 2 stays in 2nd half of new chromosome 2
+            pop2.features[j].order_id_index = mPopulation[index_p2].features[j].order_id_index;
+            pop2.features[j].on_off = mPopulation[index_p2].features[j].on_off;
+        }
+    }
+    mPopulationTemp[index_c1] = pop1;
+    mPopulationTemp[index_c2] = pop2;
+}
+
+
+/* Selection Method:
+ * 1) Calculate the sum of all score
+ * 2) pick a random number that is between 0 and sum
+ * 3) iterate through, subtracting current score from sum until
+ *    the random number is less than current score
+ * Returns: index reprsenting parent selected
+ */
+int GA::Selection()
+{
+    int sum_fitness = 0;
+    int random;
+    int index;
+
+    for (int i = 0; i < N; i++)
+    {
+        sum_fitness += mPopulation[i].fitness;
+    }
+    random = rand() % sum_fitness;
+
+    index = 0;
+    while (random >= mPopulation[index].fitness)
+    {
+        random -= mPopulation[index].fitness;
+        index++;
+    }
+
+    std::cout <<"index"<<index<<std::endl;
+    return index;
 }
 
 /*Param In: Population pop, data structure representing population of current generation
@@ -88,7 +221,6 @@ int GA::Fitness_func(population pop, sms_data data)
                 keyword[j] = ""; //Set to null 
             }
         }
-        
         /* Search for it inside SMS messeges.
         Searches through every word in sms_data.*/
         int k = 0;
@@ -106,11 +238,26 @@ int GA::Fitness_func(population pop, sms_data data)
                         {
                             score += W - j; // Index 0 of keyword is weighted at 15, Index 15 is weighted to 1
                         }
+                        else
+                        {
+                            score--;  // Evens out words commonly used in all texts
+                        }
                     }
                 }
             }
             k++;
         }
 
-    return (score/W);
+    return (score);
+}
+
+void GA::fitness_func_wrapper()
+{
+    for (int i = 0; i < N; i++)
+    {
+        /* For each population member, eval fitness */
+        mPopulation[i].fitness = Fitness_func(mPopulation[i], mData);
+
+        std::cout << "fitness: " << mPopulation[i].fitness << std::endl;
+    }
 }
--- ga.hpp
+++ ga.hpp
@@ -7,10 +7,12 @@
 #ifndef GA_HPP
 #define GA_HPP
 
+#define NUM_GENERATIONS 4
 #define ON  1
 #define OFF 0
 #define N   8       // Population size
 #define W   16      // Width of chromosome (number of features)
+//#define MAX_SPAM 747 //Total number of spam messages in training data
 
 /* CONSTANT DATA: Keywords to use as features. */
 extern std::string keywords[];
@@ -26,7 +28,7 @@ struct chromo
 {
     bool     on_off;
     int order_id_index; // Index from 0 to 15 for keyword data. Needed for inversion operations.
-    int fitness_score;
+    //int fitness_score;
 };
 
 struct population
@@ -41,14 +43,22 @@ std::string to_lower_case(std::string str_in);
 
 class GA
 {
-    public:
-    population mPopulation[N];
+    public:    
 
     GA(sms_data data);
-    int Fitness_func(population pop, sms_data data);
 
     private:
+    
+    int Fitness_func(population pop, sms_data data);
+    void fitness_func_wrapper();
+    void Crossover(int index_p1, int index_p2, int index_c1, int index_c2);
+    int Selection();
+    void Mutation();
+    void Inversion();
+    
     sms_data mData;
+    population mPopulation[N];
+    population mPopulationTemp[N];
 
 };
 
